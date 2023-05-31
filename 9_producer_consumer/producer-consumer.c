36d76d92230daa83569ce468eef51f43f1b957a8
gabdepa <gabriel-r-p-p@hotmail.com>
2023-05-31 16:48:30 -0300

    Running smoothly

--- 9_producer_consumer/producer-consumer.c
+++ 9_producer_consumer/producer-consumer.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "ppos.h"
 
-#define BUFFER_SIZE 10 // Define Buffer Size
+#define BUFFER_SIZE 10  // Define Buffer Size
 #define SLEEP_TIME 1000 // Define Sleep time
 
 // Initialize variables for last insertion and consumption
@@ -21,11 +21,11 @@ void produtor(void *arg)
     int item;
     while (1)
     {
-        task_sleep(SLEEP_TIME);                 // Sleep for a defined time
-        item = rand() % 100;              // Generate a random item
-        sem_down(&s_vacancy);             // Access vacancy semaphore
-        sem_down(&s_buffer);              // Access buffer semaphore
-        last_insertion++;                 // Increment last insertion
+        task_sleep(SLEEP_TIME);            // Sleep for a defined time
+        item = rand() % 100;               // Generate a random item
+        sem_down(&s_vacancy);              // Access vacancy semaphore
+        sem_down(&s_buffer);               // Access buffer semaphore
+        last_insertion++;                  // Increment last insertion
         if (last_insertion == BUFFER_SIZE) // Check if insertion has reached buffer limit
         {
             last_insertion = 0; // Reset last insertion
@@ -43,9 +43,9 @@ void consumidor(void *arg)
     int item;
     while (1)
     {
-        sem_down(&s_item);               // Access item semaphore
-        sem_down(&s_buffer);             // Access buffer semaphore
-        last_consumed++;                 // Increment last consumed
+        sem_down(&s_item);                // Access item semaphore
+        sem_down(&s_buffer);              // Access buffer semaphore
+        last_consumed++;                  // Increment last consumed
         if (last_consumed == BUFFER_SIZE) // Check if consumption has reached buffer limit
         {
             last_consumed = 0; // Reset last consumed
@@ -61,15 +61,15 @@ void consumidor(void *arg)
 int main()
 {
     printf("main : início\n");
-    ppos_init();                                             // Initialize ppos
-    sem_init(&s_buffer, 1);                                  // Initialize buffer semaphore
-    sem_init(&s_item, 0);                                    // Initialize item semaphore
-    sem_init(&s_vacancy, BUFFER_SIZE);                        // Initialize vacancy semaphore
+    ppos_init();                                           // Initialize ppos
+    sem_init(&s_buffer, 1);                                // Initialize buffer semaphore
+    sem_init(&s_item, 0);                                  // Initialize item semaphore
+    sem_init(&s_vacancy, BUFFER_SIZE);                     // Initialize vacancy semaphore
     task_init(&prod1, produtor, "p1");                     // Create producer task 1
     task_init(&prod2, produtor, "p2");                     // Create producer task 2
     task_init(&prod3, produtor, "p3");                     // Create producer task 3
     task_init(&cons1, consumidor, "                  c1"); // Create consumer task 1
     task_init(&cons2, consumidor, "                  c2"); // Create consumer task 2
-    task_wait(&prod1);                                       // Wait for producer task 1 to finish
-    return 0;                                                // Return 0 upon successful execution
+    task_wait(&prod1);                                     // Wait for producer task 1 to finish
+    return 0;                                              // Return 0 upon successful execution
 }

e217713314930d1cf64599ae52c7e6edeaabd814
gabdepa <gabriel-r-p-p@hotmail.com>
2023-05-31 16:46:27 -0300

    Running smoothly

--- 9_producer_consumer/producer-consumer.c
+++ 9_producer_consumer/producer-consumer.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include "ppos.h"
 
-#define TAM_BUFFER 10 // Define buffer size
+#define BUFFER_SIZE 10 // Define buffer size
 
 // Initialize variables for last insertion and consumption
 int last_insertion = -1;
 int last_consumed = -1;
 
-int buffer[TAM_BUFFER]; // Initialize buffer
+int buffer[BUFFER_SIZE]; // Initialize buffer
 
 // Initialize task variables and semaphore variables
 task_t prod1, prod2, prod3, cons1, cons2;
@@ -45,7 +45,7 @@ void consumidor(void *arg)
         sem_down(&s_item);               // Access item semaphore
         sem_down(&s_buffer);             // Access buffer semaphore
         last_consumed++;                 // Increment last consumed
-        if (last_consumed == TAM_BUFFER) // Check if consumption has reached buffer limit
+        if (last_consumed == BUFFER_SIZE) // Check if consumption has reached buffer limit
         {
             last_consumed = 0; // Reset last consumed
         }
@@ -63,7 +63,7 @@ int main()
     ppos_init();                                             // Initialize ppos
     sem_init(&s_buffer, 1);                                  // Initialize buffer semaphore
     sem_init(&s_item, 0);                                    // Initialize item semaphore
-    sem_init(&s_vacancy, TAM_BUFFER);                        // Initialize vacancy semaphore
+    sem_init(&s_vacancy, BUFFER_SIZE);                        // Initialize vacancy semaphore
     task_init(&prod1, produtor, "p1");                     // Create producer task 1
     task_init(&prod2, produtor, "p2");                     // Create producer task 2
     task_init(&prod3, produtor, "p3");                     // Create producer task 3
